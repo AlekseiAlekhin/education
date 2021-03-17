70f3a5170619ee37cb2a9831e17efff5573a35b4
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-17 16:52:03 +0300

    changes in sum-check/index.html

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,5 +1,12 @@
-function sumCheck(a, b) {
-  // Напиши свой код здесь
+function sumCheck(a, b) { 
+  let sum;
+  if ((typeof(a) == 'number' && typeof(b) == 'number')){
+    sum = a + b;
+    return sum;
+  }else{
+    throw new Error('Wrong arguments type!');
+  }
+  
 }
 
 window.sumCheck = sumCheck;
