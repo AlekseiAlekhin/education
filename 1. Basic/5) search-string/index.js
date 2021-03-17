9f8515993457419f2eb41fc169d396e5e0b3c942
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-17 13:43:10 +0300

    changes in search-string/index.js

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,5 +1,17 @@
 function searchString(obj) {
-  // Напиши свой код здесь
+  let keys = Object.keys(obj);
+  let values = Object.values(obj);
+  let stringObj = "?";
+  if (keys.length == 0 || values.length == 0) {
+    return (stringObj = "");
+  }
+
+  for (let key in obj) {
+    if (!["", null, undefined].includes(obj[key])) {
+      stringObj = stringObj + `${key}=${obj[key]}&`;
+    }
+  }
+  return stringObj.slice(0, -1);
 }
 
 window.searchString = searchString;
