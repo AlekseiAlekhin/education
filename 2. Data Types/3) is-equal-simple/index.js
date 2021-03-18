8a4668b412b5e57eaf02e969f5916925ba62b292
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-18 12:15:41 +0300

    is-equal-simple

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,7 +1,18 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
-}
+  let stringA;
+  let stringB;
+  for (let key in a) {
+    stringA = stringA + `${key}${a[key]}`;
+  }
+  for (let key in b) {
+    stringB = stringB + `${key}${b[key]}`;
+  }
+  if (null == a || null == b) {
+    return false;
+  }
 
+  return stringA == stringB;
+}
 window.isEqual = isEqual;
 
 export default isEqual;
