1ec59604ae777b5a0deaefdd4099ec2db588a5fc
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-22 17:47:44 +0300

    async-sum

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,5 +1,9 @@
 function asyncSum(a, b) {
-  // Напиши свой код здесь
+    return new Promise(function (resolve, reject) {
+        (typeof (a) === 'number' && typeof (b) === 'number') ?
+            setTimeout(() => resolve(a + b), 100) :
+            reject(new Error('error'));
+    })
 }
 
 window.asyncSum = asyncSum;
