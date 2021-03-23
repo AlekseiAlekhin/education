96ee3f02fe8a41635beff7948a226230bf10bb03
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-23 15:50:47 +0300

    xhr-get

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,16 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+    let promise = new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.responseType = 'json';
+        xhr.onload = () => {
+            (xhr.status >= 200 && xhr.status < 300) ? resolve(xhr.response) :
+                reject(xhr.response);
+        };
+        xhr.send();
+    })
+    return promise;
 }
 
 window.xhrGet = xhrGet;
