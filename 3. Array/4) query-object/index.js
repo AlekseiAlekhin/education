ae0609da303ea5eaf88e4ac694a67299a290d2b1
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-23 10:36:40 +0300

    query-object last fix

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,13 +1,12 @@
 import stringToType from '/home/user/work/education/2. Data Types/1) string-to-type/index.js';
 
 function queryToObject(query) {
-    const obj = {};
     const arr = query.slice(1).split('&');
     const array = arr.map((item) => item.split('='));
-    array.reduce(function (item, index) {
-       obj[index[0]] = stringToType(index[1]);
-    }, 0);
-    return obj;
+    return array.reduce(function (accum, item) {
+        accum[item[0]] = stringToType(item[1]);
+        return accum;
+    }, {});
 }
 
 window.queryToObject = queryToObject;
