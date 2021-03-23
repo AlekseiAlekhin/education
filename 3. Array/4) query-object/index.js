b19d2b88ee3915a2001cfd99b887980422ef3439
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-23 09:31:06 +0300

    query-object fix

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,28 +1,13 @@
+import {stringToType} from '/home/user/work/education/2. Data Types/1) string-to-type/index.js';
+
 function queryToObject(query) {
     const obj = {};
     const arr = query.slice(1).split('&');
     console.log(arr)
     const array = arr.map((item) => item.split('='))
     console.log('array', array)
-    array.forEach(function (item, index) {
-        if (Number(item[1])) {
-            obj[item[0]] = parseInt(item[1], 10);
-        } else {
-            switch (item[1]) {
-                case "true":
-                    obj[item[0]] = true;
-                    break;
-                case "false":
-                    obj[item[0]] = false;
-                    break;
-                case "0":
-                    obj[item[0]] = 0;
-                    break;
-                default:
-                    obj[item[0]] = item[1];
-                    break;
-            }
-        }
+    array.reduce(function (item) {
+       return obj[item[0]] = stringToType(item[1]);
     });
     return obj;
 }
