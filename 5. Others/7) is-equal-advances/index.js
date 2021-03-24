19e89bf36d399138d27e6a1a84df0e1210c93357
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-24 16:50:05 +0300

    for vadim

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -1,5 +1,11 @@
+ import isEqual from '/home/user/work/education/2. Data Types/3) is-equal-simple/index.js';
+
 function isEqualAdvances(a, b) {
-  // Напиши свой код здесь
+    if (a === null || b === null || Object.values(a).includes(undefined) || Object.values(b).includes(undefined)){
+        return isEqual(a, b);
+    }else{
+       return JSON.stringify(a) === JSON.stringify(b);
+    }
 }
 
 window.isEqualAdvances = isEqualAdvances;
