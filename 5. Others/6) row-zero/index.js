985656016ac190625f905255d454a1f42c6e0295
AlekseiAlekhin <alekhin.exceedteam@gmail.com>
2021-03-26 15:32:48 +0300

    row-zero fix

--- 5. Others/6) row-zero/index.js
+++ 5. Others/6) row-zero/index.js
@@ -1,13 +1,11 @@
 function rowZero(str) {
     if (/1+/.test(str) && !/0+/.test(str) || str === '') {
         return 0
-    } else {
-        return str.match(/0+/g).reduce(function (accum, item) {
-            return (accum.length < item.length) ? item : accum;
-        }).length;
     }
+    return str.match(/0+/g).reduce(function (accum, item) {
+        return (accum.length < item.length) ? item : accum;
+    }).length;
 }
-
 
 window.rowZero = rowZero;
 
